79ea26118adf7caf2208b8aec4746be7483e3047
DarrenKwonDev <anatomy1545@gmail.com>
2022-04-08 21:23:39 +0900

    feat: dynamic json import

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 import settings from "./.vscode/settings.json" assert { type: "json" };
 
+const configuration = await import("./deno.json", { assert: { type: "json" } });
+
 console.log(settings["deno.enable"]);
+console.log(configuration);
