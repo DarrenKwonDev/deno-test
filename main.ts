29c882a9948ebec985479c21d4ea457f7b62051b
DarrenKwonDev <anatomy1545@gmail.com>
2022-04-08 19:01:39 +0900

    feat: file read

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
-const url = Deno.args[0];
-const res = await fetch(url);
-
-const body = new Uint8Array(await res.arrayBuffer());
-await Deno.stdout.write(body);
+import { copy } from "https://deno.land/std@0.134.0/streams/conversion.ts";
+const filenames = Deno.args;
+for (const filename of filenames) {
+  const file = await Deno.open(filename);
+  await copy(file, Deno.stdout);
+  file.close();
+}
