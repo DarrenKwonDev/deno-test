e765b73e4cf15a4e84bcf6977a07c2b3db6b3fc9
DarrenKwonDev <anatomy1545@gmail.com>
2022-04-08 21:52:13 +0900

    feat: command line arguments

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
-console.log(Deno.env.get("PORT")); // undefined
-Deno.env.set("my_secret", "I have a secret");
+import { parse } from "https://deno.land/std@0.119.0/flags/mod.ts";
 
-console.log(Deno.env.get("my_secret"));
-console.log(Deno.env.toObject()); // get all env
+console.log("Deno.args", Deno.args);
+
+const flags = parse(Deno.args, {
+  boolean: ["help", "color"],
+  string: ["version"],
+  default: { color: true },
+});
+
+console.log("Wants help?", flags.help);
+console.log("Version:", flags.version);
+console.log("Wants color?:", flags.color);
+
+console.log("Other:", flags._); // [ "Deno", "Sushi" ]
